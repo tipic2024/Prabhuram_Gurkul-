fbe7921e0fc55419f4455c1caaaa88665490f006
Kunal-Tipic <kunal.zende@tipic.in>
2024-09-11 13:55:22 +0530

    About us frontend done

--- src/SchoolWebsite/About.js
+++ src/SchoolWebsite/About.js
@@ -9,10 +9,10 @@ function AboutD({ About }) {
       <div className="w-[95%] flex flex-col md:flex-row">
         {isOdd ? (
           <>
-            <div className="md:w-1/2 h-64 md:h-auto">
+            <div className=" w-full md:w-1/2 h-64 md:h-auto">
               <img src={imagePath} className="object-scale-down md:object-cover w-full h-full" alt={About.name} />
             </div>
-            <div className="w-full md:w-1/2 flex flex-col lg:mt-14 md:pl-16">
+            <div className="order-1 md:order-2 w-full md:w-1/2 flex flex-col lg:mt-14 md:pl-16">
               <div>
                 <h1 className="text-sm uppercase text-left"></h1>
                 <h1 className="font-serif text-3xl mt-4 text-center pb-3">{About.name}</h1>
